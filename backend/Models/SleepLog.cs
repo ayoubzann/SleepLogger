c4296c682ef255b78b82866359b8348a977913de
ayoubzann <ayoubzann@gmail.com>
2024-01-05 11:18:19 +0100

    Added: SleepLog-Model

---
+++ backend/Models/SleepLog.cs
@@ -0,0 +1,8 @@
+public class SleepLog
+{
+    public double Duration { get; set; }
+    public string BedTime { get; set; }
+    public string WakeTime { get; set; }
+    public int RestfullnessScale { get; set; }
+
+}
